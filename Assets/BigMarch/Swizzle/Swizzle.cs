8ce23a666c1bb05a7d36916806bfa09788d89bb5
yuenze <yuenze@coolfishgames.com>
2017-04-28 17:26:55 +0800

    可以说unity不集成这个功能到引擎里就不是中国人了。

--- Assets/BigMarch/Swizzle/Swizzle.cs
+++ Assets/BigMarch/Swizzle/Swizzle.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿// 不但实现了unity CG shader中的swizzle，还扩展了0和1。
+// 只需轻轻一句v = v.Swizzle_x0z(); 即可轻松应对 v = new Vector3(v.x, 0, v.z); 的情况。
+// 如果想把transform放到地面，只需transform.position = transform.position.Swizzle_x0z()。
+
+using UnityEngine;
 
 namespace BigMarch.Swizzle
 {
