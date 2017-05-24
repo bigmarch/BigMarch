5b4d4935779fcdda4c19c9d3708f2b07c3558265
yuenze <yuenze@coolfishgames.com>
2017-05-24 19:00:30 +0800

    两个工具方法

--- Assets/BigMarch/Tool/Utility.cs
+++ Assets/BigMarch/Tool/Utility.cs
@@ -31,5 +31,19 @@ namespace BigMarch.Tool
 			result.y = Mathf.LerpAngle(a.y, b.y, t);
 			return result;
 		}
+
+		#region component extension
+
+		public static RectTransform RectTransform(this Component cp)
+		{
+			return cp.transform as RectTransform;
+		}
+
+		public static float Remap(this float value, float from1, float to1, float from2, float to2)
+		{
+			return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+		}
+
+		#endregion
 	}
 }
