10e7c32bc5beb3ccdc6249ba634b5d5fb23e598b
yuenze <yuenze@coolfishgames.com>
2017-12-12 12:12:20 +0800

    新增MultiTaskDivider，把重型运算分摊到多帧。

---
+++ Assets/BigMarch/MultiTaskDivider/MultiTaskDivider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+
+public class MultiTaskDivider
+{
+	private class Task
+	{
+		// 这个任务的唯一标识，用于过滤重复的任务。
+		public string TaskId;
+		// 任务的具体逻辑
+		public Action TaskAction;
+	}
+
+	// dic 的作用，紧紧是为了过滤重复任务。
+	private Dictionary<string, Task> _taskDic = new Dictionary<string, Task>();
+
+	// 任务队列
+	private Queue<Task> _queue = new Queue<Task>();
+
+	public void AddTaskIfNotExist(string taskId, Action taskAction)
+	{
+		// 如果是个新的 task，那么就加到 dic 和 list 中。
+		if (!_taskDic.ContainsKey(taskId))
+		{
+			Task newTask = new Task();
+			newTask.TaskId = taskId;
+			newTask.TaskAction = taskAction;
+
+			_taskDic.Add(taskId, newTask);
+
+			_queue.Enqueue(newTask);
+
+//			Debug.Log("MultiTaskDivider : add new task " + taskId);
+		}
+	}
+
+	public void ExecuteOne()
+	{
+		if (_queue.Count == 0)
+		{
+			return;
+		}
+
+		// 拿出来一个任务，执行，然后放到队尾，继续排队。
+		Task taskGoingToDo = _queue.Dequeue();
+		taskGoingToDo.TaskAction();
+		_queue.Enqueue(taskGoingToDo);
+	}
+}
+
---
+++ Assets/BigMarch/MultiTaskDividerExample/MultiTaskDividerExample.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiTaskDividerExample : MonoBehaviour
+{
+	public bool UseMultiTaskDivider = false;
+
+	public Transform[] RayCastPointArr;
+
+	private MultiTaskDivider _multiTaskDivider = new MultiTaskDivider();
+
+	// Update is called once per frame
+	void Update()
+	{
+		Debug.Log(Time.frameCount + "  frame start--------------------------------------------------");
+		if (UseMultiTaskDivider)
+		{
+			Update_UseMultiTaskDivider();
+		}
+		else
+		{
+			Update_NotUseMultiTaskDivider();
+		}
+	}
+
+	private void Update_UseMultiTaskDivider()
+	{
+		foreach (Transform t0 in RayCastPointArr)
+		{
+			foreach (Transform t1 in RayCastPointArr)
+			{
+				if (t0 == t1)
+				{
+					continue;
+				}
+				DoAnything(t0, t1);
+			}
+		}
+	}
+
+	private void Update_NotUseMultiTaskDivider()
+	{
+		foreach (Transform t0 in RayCastPointArr)
+		{
+			foreach (Transform t1 in RayCastPointArr)
+			{
+				if (t0 == t1)
+				{
+					continue;
+				}
+
+				var t0_copy = t0;
+				var t1_copy = t1;
+				_multiTaskDivider.AddTaskIfNotExist(
+					t0.name + "-" + t1.name,
+					() => { DoAnything(t0_copy, t1_copy); });
+			}
+		}
+		_multiTaskDivider.ExecuteOne();
+	}
+
+	private void DoAnything(Transform t0, Transform t1)
+	{
+		Debug.DrawLine(t0.position, t1.position, Color.yellow);
+		Debug.Log("Draw line " + t0.name + " " + t1.name);
+	}
+}
