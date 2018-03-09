025411f4cbc0aef268bdf904a3ebc85f0434684c
big_march <big_march@foxmail.com>
2018-03-09 17:26:39 +0800

    新增没有什么大用的，不传上来就难受的，自己发明创造的，frame blend效果。

--- Assets/BigMarch/RenderTrick/FrameBlendImageEffect/FrameBlendImageEffect.cs
+++ Assets/BigMarch/RenderTrick/FrameBlendImageEffect/FrameBlendImageEffect.cs
@@ -9,7 +9,6 @@ public class FrameBlendImageEffect : MonoBehaviour
 	public bool DownSample = true;
 	public int MaxRenderTextureHeight = 800;
 
-	public bool FrameBlendOn = true;
 	public float BlendRatio = .333f;
 
 	public RenderTexture Rt0;
@@ -54,34 +53,26 @@ public class FrameBlendImageEffect : MonoBehaviour
 
 	void OnPreRender()
 	{
-		if (FrameBlendOn)
+		if (Rt0 == null || Rt1 == null)
 		{
-			if (Rt0 == null || Rt1 == null)
-			{
-				return;
-			}
-			switch (_renderStep)
-			{
-				case RenderStep.Rt0:
-					break;
-				case RenderStep.Rt0ToRt1:
-					MainCamera.targetTexture = Rt1;
-					MainCamera.Render();
-					break;
-				case RenderStep.Rt1:
-					break;
-				case RenderStep.Rt1ToRt0:
-					MainCamera.targetTexture = Rt0;
-					MainCamera.Render();
-					break;
-				default:
-					throw new ArgumentOutOfRangeException();
-			}
+			return;
 		}
-		else
+		switch (_renderStep)
 		{
-			MainCamera.targetTexture = Rt0;
-			MainCamera.Render();
+			case RenderStep.Rt0:
+				break;
+			case RenderStep.Rt0ToRt1:
+				MainCamera.targetTexture = Rt1;
+				MainCamera.Render();
+				break;
+			case RenderStep.Rt1:
+				break;
+			case RenderStep.Rt1ToRt0:
+				MainCamera.targetTexture = Rt0;
+				MainCamera.Render();
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
 		}
 	}
 
@@ -98,13 +89,7 @@ public class FrameBlendImageEffect : MonoBehaviour
 				width = (int) (MaxRenderTextureHeight * aspect);
 				height = MaxRenderTextureHeight;
 			}
-			else
-			{
-				width = source.width;
-				height = source.height;
-			}
 		}
-
 
 		if (Rt0 == null || Rt0.width != width || Rt0.height != height)
 		{
@@ -120,65 +105,59 @@ public class FrameBlendImageEffect : MonoBehaviour
 			Rt1.filterMode = FilterMode.Bilinear;
 		}
 
-		if (FrameBlendOn)
+
+		switch (_renderStep)
 		{
-			switch (_renderStep)
-			{
-				case RenderStep.Rt0:
-					//用于blend的texture进行down sample
-					//blurbuffer = RenderTexture.GetTemporary(Rt1.width / 4, Rt1.height / 4, 0);
-					//Graphics.Blit(Rt1, blurbuffer);
+			case RenderStep.Rt0:
+				//用于blend的texture进行down sample
+				//blurbuffer = RenderTexture.GetTemporary(Rt1.width / 4, Rt1.height / 4, 0);
+				//Graphics.Blit(Rt1, blurbuffer);
 
-					_frameBlendMaterial.SetTexture("_BlendTex", Rt1);
-					_frameBlendMaterial.SetFloat("_BlendRatio", BlendRatio);
+				_frameBlendMaterial.SetTexture("_BlendTex", Rt1);
+				_frameBlendMaterial.SetFloat("_BlendRatio", BlendRatio);
 
-					Graphics.Blit(Rt0, destination, _frameBlendMaterial);
-					_renderStep = RenderStep.Rt0ToRt1;
-					//RenderTexture.ReleaseTemporary(blurbuffer);
-					break;
-				case RenderStep.Rt0ToRt1:
-					//用于blend的texture进行down sample
-					//blurbuffer = RenderTexture.GetTemporary(Rt0.width/4, Rt0.height/4, 0);
-					//Graphics.Blit(Rt0, blurbuffer);
+				Graphics.Blit(Rt0, destination, _frameBlendMaterial);
+				_renderStep = RenderStep.Rt0ToRt1;
+				//RenderTexture.ReleaseTemporary(blurbuffer);
+				break;
+			case RenderStep.Rt0ToRt1:
+				//用于blend的texture进行down sample
+				//blurbuffer = RenderTexture.GetTemporary(Rt0.width/4, Rt0.height/4, 0);
+				//Graphics.Blit(Rt0, blurbuffer);
 
-					_frameBlendMaterial.SetTexture("_BlendTex", Rt0);
-					_frameBlendMaterial.SetFloat("_BlendRatio", 1 - BlendRatio);
+				_frameBlendMaterial.SetTexture("_BlendTex", Rt0);
+				_frameBlendMaterial.SetFloat("_BlendRatio", 1 - BlendRatio);
 
-					Graphics.Blit(Rt1, destination, _frameBlendMaterial);
-					_renderStep = RenderStep.Rt1;
-					//RenderTexture.ReleaseTemporary(blurbuffer);
-					break;
-				case RenderStep.Rt1:
-					//用于blend的texture进行down sample
-					//				blurbuffer = RenderTexture.GetTemporary(Rt0.width / 4, Rt0.height / 4, 0);
-					//				Graphics.Blit(Rt0, blurbuffer);
+				Graphics.Blit(Rt1, destination, _frameBlendMaterial);
+				_renderStep = RenderStep.Rt1;
+				//RenderTexture.ReleaseTemporary(blurbuffer);
+				break;
+			case RenderStep.Rt1:
+				//用于blend的texture进行down sample
+				//				blurbuffer = RenderTexture.GetTemporary(Rt0.width / 4, Rt0.height / 4, 0);
+				//				Graphics.Blit(Rt0, blurbuffer);
 
-					_frameBlendMaterial.SetTexture("_BlendTex", Rt0);
-					_frameBlendMaterial.SetFloat("_BlendRatio", BlendRatio);
+				_frameBlendMaterial.SetTexture("_BlendTex", Rt0);
+				_frameBlendMaterial.SetFloat("_BlendRatio", BlendRatio);
 
-					Graphics.Blit(Rt1, destination, _frameBlendMaterial);
-					_renderStep = RenderStep.Rt1ToRt0;
-					//				RenderTexture.ReleaseTemporary(blurbuffer);
-					break;
-				case RenderStep.Rt1ToRt0:
-					//用于blend的texture进行down sample
-					//blurbuffer = RenderTexture.GetTemporary(Rt1.width / 4, Rt1.height / 4, 0);
-					//Graphics.Blit(Rt1, blurbuffer);
+				Graphics.Blit(Rt1, destination, _frameBlendMaterial);
+				_renderStep = RenderStep.Rt1ToRt0;
+				//				RenderTexture.ReleaseTemporary(blurbuffer);
+				break;
+			case RenderStep.Rt1ToRt0:
+				//用于blend的texture进行down sample
+				//blurbuffer = RenderTexture.GetTemporary(Rt1.width / 4, Rt1.height / 4, 0);
+				//Graphics.Blit(Rt1, blurbuffer);
 
-					_frameBlendMaterial.SetTexture("_BlendTex", Rt1);
-					_frameBlendMaterial.SetFloat("_BlendRatio", 1 - BlendRatio);
+				_frameBlendMaterial.SetTexture("_BlendTex", Rt1);
+				_frameBlendMaterial.SetFloat("_BlendRatio", 1 - BlendRatio);
 
-					Graphics.Blit(Rt0, destination, _frameBlendMaterial);
-					_renderStep = RenderStep.Rt0;
-					//				RenderTexture.ReleaseTemporary(blurbuffer);
-					break;
-				default:
-					throw new ArgumentOutOfRangeException();
-			}
-		}
-		else
-		{
-			Graphics.Blit(Rt0, destination);
+				Graphics.Blit(Rt0, destination, _frameBlendMaterial);
+				_renderStep = RenderStep.Rt0;
+				//				RenderTexture.ReleaseTemporary(blurbuffer);
+				break;
+			default:
+				throw new ArgumentOutOfRangeException();
 		}
 	}
 
