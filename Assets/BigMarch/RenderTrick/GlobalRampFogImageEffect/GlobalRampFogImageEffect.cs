d340f01f43bbfb290cc8514b1b5993a6c45605e2
big_march <big_march@foxmail.com>
2018-04-19 20:42:45 +0800

    没有 height 的 global fog 不能叫作 global fog。

---
+++ Assets/BigMarch/RenderTrick/GlobalRampFogImageEffect/GlobalRampFogImageEffect.cs
@@ -0,0 +1,112 @@
+﻿#pragma warning disable 3001, 3002, 3003, 3005, 3008, 3009, 3024
+using System;
+using UnityEngine;
+
+//	[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class GlobalRampFogImageEffect : MonoBehaviour
+{
+	public float Multiplier = 1;
+	public Texture RampTexture;
+	public Vector2 StartEnd = new Vector2(0, 50);
+	public Shader FogShader = null;
+	private Material _fogMaterial = null;
+
+	void Awake()
+	{
+		FogShader = Shader.Find("Hidden/GlobalRampFog");
+	}
+
+	void OnEnable()
+	{
+		_fogMaterial = new Material(FogShader);
+	}
+
+	void OnDisable()
+	{
+		DestroyImmediate(_fogMaterial);
+	}
+
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		/*Camera cam = GetComponent<Camera>();
+		Transform camtr = cam.transform;
+		float camNear = cam.nearClipPlane;
+		float camFar = cam.farClipPlane;
+		float camFov = cam.fieldOfView;
+		float camAspect = cam.aspect;
+
+		Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+		float fovWHalf = camFov * 0.5f;
+
+		Vector3 toRight = camtr.right * camNear * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camAspect;
+		Vector3 toTop = camtr.up * camNear * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+		Vector3 topLeft = (camtr.forward * camNear - toRight + toTop);
+		float camScale = topLeft.magnitude * camFar / camNear;
+
+		topLeft.Normalize();
+		topLeft *= camScale;
+
+		Vector3 topRight = (camtr.forward * camNear + toRight + toTop);
+		topRight.Normalize();
+		topRight *= camScale;
+
+		Vector3 bottomRight = (camtr.forward * camNear + toRight - toTop);
+		bottomRight.Normalize();
+		bottomRight *= camScale;
+
+		Vector3 bottomLeft = (camtr.forward * camNear - toRight - toTop);
+		bottomLeft.Normalize();
+		bottomLeft *= camScale;
+
+		frustumCorners.SetRow(0, topLeft);
+		frustumCorners.SetRow(1, topRight);
+		frustumCorners.SetRow(2, bottomRight);
+		frustumCorners.SetRow(3, bottomLeft);
+
+		var camPos = camtr.position;
+		_fogMaterial.SetMatrix("_FrustumCornersWS", frustumCorners);
+		_fogMaterial.SetVector("_CameraWS", camPos);*/
+
+		_fogMaterial.SetVector("_DistanceParams", new Vector4(StartEnd.x, StartEnd.y, 0, 0));
+		_fogMaterial.SetTexture("_RampTexture", RampTexture);
+
+		_fogMaterial.SetTexture("_DepthTex", GetComponent<BackUpFirstPostEffectSourceRenderTexture>().DepthRt);
+		_fogMaterial.SetFloat("_Multiplier", Multiplier);
+
+		Graphics.Blit(source, destination, _fogMaterial);
+//		CustomGraphicsBlit(source, destination, _fogMaterial, 0);
+	}
+
+//	static void CustomGraphicsBlit(RenderTexture source, RenderTexture dest, Material fxMaterial, int passNr)
+//	{
+//		RenderTexture.active = dest;
+//
+//		fxMaterial.SetTexture("_MainTex", source);
+//
+//		GL.PushMatrix();
+//		GL.LoadOrtho();
+//
+//		fxMaterial.SetPass(passNr);
+//
+//		GL.Begin(GL.QUADS);
+//
+//		GL.MultiTexCoord2(0, 0.0f, 0.0f);
+//		GL.Vertex3(0.0f, 0.0f, 3.0f); // BL
+//
+//		GL.MultiTexCoord2(0, 1.0f, 0.0f);
+//		GL.Vertex3(1.0f, 0.0f, 2.0f); // BR
+//
+//		GL.MultiTexCoord2(0, 1.0f, 1.0f);
+//		GL.Vertex3(1.0f, 1.0f, 1.0f); // TR
+//
+//		GL.MultiTexCoord2(0, 0.0f, 1.0f);
+//		GL.Vertex3(0.0f, 1.0f, 0.0f); // TL
+//
+//		GL.End();
+//		GL.PopMatrix();
+//	}
+}
+
---
+++ Assets/BigMarch/RenderTrick/GlobalRampFogImageEffect/Hidden-GlobalRampFog.shader
@@ -0,0 +1,94 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Hidden/GlobalRampFog" 
+{
+	Properties 
+	{
+		_MainTex("Base (RGB)", 2D) = "white" {}
+	}
+	SubShader 
+	{
+		Pass
+		{
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			uniform float4 _MainTex_TexelSize;
+			uniform half4 _DistanceParams;
+
+			// for fast world space reconstruction
+			uniform float4x4 _FrustumCornersWS;
+			uniform float4 _CameraWS;
+			uniform sampler2D _DepthTex;
+			uniform sampler2D _RampTexture;
+			uniform float _Multiplier;
+
+			struct v2f {
+				float4 pos : SV_POSITION;
+				float2 uv : TEXCOORD0;
+				float2 uv_depth : TEXCOORD1;
+				float4 interpolatedRay : TEXCOORD2;
+			};
+
+			v2f vert(appdata_img v)
+			{
+				v2f o;
+				half index = v.vertex.z;
+				v.vertex.z = 0.1;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = v.texcoord.xy;
+				o.uv_depth = v.texcoord.xy;
+
+		#if UNITY_UV_STARTS_AT_TOP
+				if (_MainTex_TexelSize.y < 0)
+					o.uv.y = 1 - o.uv.y;
+		#endif				
+				o.interpolatedRay = _FrustumCornersWS[(int)index];
+				o.interpolatedRay.w = index;
+
+				return o;
+			}
+
+			float Remap(float val, float iMin, float iMax, float oMin, float oMax)
+			{
+				return oMin + (val - iMin) * (oMax - oMin) / (iMax - iMin);
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				//return fixed4(1,0,0,1);
+				float linearDepth = LinearEyeDepth(SAMPLE_DEPTH_TEXTURE(_DepthTex, i.uv_depth));
+				//float linearDepth = Linear01Depth(SAMPLE_DEPTH_TEXTURE(_DepthTex, i.uv_depth));
+
+				//return linearDepth.rrrr;
+				float u = Remap(linearDepth, _DistanceParams.x, _DistanceParams.y, 0, 1);
+				u = saturate(u);
+				//return u.xxxx;
+
+				fixed4 fogColor = tex2D(_RampTexture, float2(u, 0.5f));
+
+				fixed4 original = tex2D(_MainTex, i.uv);
+				fixed3 rgb = lerp(original.rgb, fogColor.rgb, fogColor.a*_Multiplier);
+
+				return fixed4(rgb, original.a);
+
+				/*float3 worldPos = _WorldSpaceCameraPos + linearDepth * i.interpolatedRay.xyz;
+
+				float fogDensity = (_FogEnd - worldPos.y) / (_FogEnd - _FogStart);
+				fogDensity = saturate(fogDensity * _FogDensity);
+
+				fixed4 finalColor = tex2D(_MainTex, i.uv);
+				finalColor.rgb = lerp(finalColor.rgb, _FogColor.rgb, fogDensity);
+
+				return finalColor;*/
+			}
+
+			ENDCG
+		}						
+	}
+	FallBack Off
+}
