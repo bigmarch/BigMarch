83ca70851f95a664e0b3ba3ec4157e67260b0217
big_march <big_march@foxmail.com>
2018-04-20 18:04:51 +0800

    给坦克游戏做的非常粗糙的链儿子钩儿。

---
+++ Assets/BigMarch/HookChain/HookChain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookChain : MonoBehaviour
+{
+	public Vector2 UvSpeed = Vector2.zero;
+	public Vector3 StartPoint = new Vector3(0, 0, 10);
+	public Vector3 EndPoint = new Vector3(0, 0, 0);
+
+	private LineRenderer _lr;
+
+	void OnEnable()
+	{
+		_lr = GetComponentInChildren<LineRenderer>();
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (_lr)
+		{
+			_lr.SetPosition(0, StartPoint);
+			_lr.SetPosition(1, EndPoint);
+
+			if (UvSpeed != Vector2.zero)
+			{
+				_lr.material.SetTextureOffset("_MainTex", UvSpeed * Time.time);
+			}
+		}
+	}
+}
