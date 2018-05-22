51219a8b53c83e3a3bd75917581b40235afe68f7
big_march <big_march@foxmail.com>
2018-05-22 21:06:28 +0800

    基于 ZoomInImageEffect 的震动效果。

---
+++ Assets/BigMarch/RenderTrick/ZoomInImageEffect/ZoomInImageEffectShake.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomInImageEffectShake : MonoBehaviour
+{
+	public float LerpSpeed = 1;
+	public float ShakeDelta = .02f;
+	private ZoomInImageEffect _effect;
+	
+	void Awake()
+	{
+		_effect = GetComponent<ZoomInImageEffect>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		_effect.ZoomInFactor = Mathf.Lerp(_effect.ZoomInFactor, 1, Time.deltaTime * LerpSpeed);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Shake();
+		}
+	}
+
+	public void Shake()
+	{
+		_effect.ZoomInFactor -= ShakeDelta;
+	}
+}
