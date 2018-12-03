658eaf0590699bb19a984133aa2f7661eb962de6
big_march <big_march@foxmail.com>
2018-12-03 15:07:26 +0800

    set dirty tool

---
+++ Assets/BigMarch/Tool/Editor/SetDirty.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+public class SetDirty
+{
+	[MenuItem("BigMarch/SetDirty")]
+	public static void Do()
+	{
+		var all = Selection.objects;
+		foreach (var o in all)
+		{
+			EditorUtility.SetDirty(o);
+		}
+
+		AssetDatabase.SaveAssets();
+	}
+}
